eec2d70d9ec29661b62a435f866f32884cdcf633
Ryan C. Thompson <rct@thompsonclan.org>
2011-02-12 23:27:20 -0800

    Ensure my perl alias takes precedence

--- site-lisp/settings/sepia-settings.el
+++ site-lisp/settings/sepia-settings.el
@@ -1,5 +1,10 @@
 ;;; Sepia mode (Part of Simple Emacs Perl InterAction),
 ;; derived from cperl-mode, so replaces it
+
+;; Require these first, so that my alias below overwrites any aliases they
+;; might set up.
+(require 'perl-mode)
+(require 'cperl-mode)
 (require 'sepia)
 (defalias 'perl-mode 'sepia-mode)
 
