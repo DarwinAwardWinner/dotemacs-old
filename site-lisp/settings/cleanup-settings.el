fb0a91fb203488c3ddd50ee7930c91d062d1947d
Ryan C. Thompson <rct@thompsonclan.org>
2011-03-30 13:00:18 -0700

    Add cleanup settings

---
+++ site-lisp/settings/cleanup-settings.el
@@ -0,0 +1,11 @@
+(defun clean-up-buffer-or-region ()
+  "Untabifies, indents and deletes trailing whitespace from buffer or region."
+  (interactive)
+  (save-excursion
+    (unless (region-active-p)
+      (mark-whole-buffer))
+    (untabify (region-beginning) (region-end))
+    (indent-region (region-beginning) (region-end))
+    (save-restriction
+      (narrow-to-region (region-beginning) (region-end))
+      (delete-trailing-whitespace))))
