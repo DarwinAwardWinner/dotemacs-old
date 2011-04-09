4af5d93197c1c23a098797d786ea22f3f9001446
Ryan C. Thompson <rct@thompsonclan.org>
2011-04-08 18:40:15 -0700

    Add whitespace to docstring

--- site-lisp/packages/defadvice-let.el
+++ site-lisp/packages/defadvice-let.el
@@ -2,6 +2,7 @@
 
 (defmacro defadvice-let (varlist fn &optional docstring)
   "Wrap FN in a dynamic binding of VARLIST.
+
 DOCSTRING, if provided, becomes the docstring of the advice."
   `(defadvice ,fn (around ,(gensym "defadvice-let") activate)
      ,@(funcall (if docstring
