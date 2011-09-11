25141f9c5077bc2afee6b122c3a109a9bc346ef3
Ryan C. Thompson <rct@thompsonclan.org>
2011-09-11 11:08:40 -0700

    Prevent auto-mark-executable from raising errors in various conditions

--- site-lisp/settings/executable-settings.el
+++ site-lisp/settings/executable-settings.el
@@ -1,6 +1,16 @@
 (eval-after-load 'executable
   '(progn
      (defadvice executable-make-buffer-file-executable-if-script-p (around skip-backup-files activate)
+       "Don't operate on backup files or auto-save files."
        (unless (or (auto-save-file-name-p (buffer-file-name))
                    (backup-file-name-p (buffer-file-name)))
-         ad-do-it))))
+         ad-do-it))
+
+     (defadvice executable-make-buffer-file-executable-if-script-p (around skip-tramp-files activate)
+       "Don't operate on remote files."
+       (unless (tramp-tramp-file-p (buffer-file-name))
+         ad-do-it))
+
+     (defadvice executable-make-buffer-file-executable-if-script-p (around noerror activate)
+       "Don't complain if it fails."
+       (ignore-errors ad-do-it))))
