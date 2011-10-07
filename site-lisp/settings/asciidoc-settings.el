7949920059b33479bcf0f414153eddddbcc1b29b
Ryan C. Thompson <rct@thompsonclan.org>
2011-10-07 00:29:27 -0700

    Add asciidoc settings

---
+++ site-lisp/settings/asciidoc-settings.el
@@ -0,0 +1,5 @@
+(eval-after-load 'doc-mode
+  '(progn
+     (add-to-list 'auto-mode-alist '("\\.[Aa][Dd][Oo][Cc]$" . doc-mode))
+     (add-to-list 'auto-mode-alist '("\\.[Aa][Ss][Cc][Ii][Ii][Dd][Oo][Cc]$" . doc-mode))))
+(require 'asciidoc nil 'noerror)
