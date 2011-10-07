9757ea6652611aab0016afdac7e8ff58e9db8703
Ryan C. Thompson <rct@thompsonclan.org>
2011-10-07 00:05:25 -0700

    Ido-ubiquitous is now its own package
    So remove it from ido-settings.el

--- site-lisp/settings/ido-settings.el
+++ site-lisp/settings/ido-settings.el
@@ -1,25 +1,6 @@
 (require 'ido)
 (load "ido-other-window" 'noerror)
-
-;; Replace completing-read wherever possible, unless directed otherwise
-(defadvice completing-read (around use-ido-when-possible activate)
-  "If `ido-everywhere' is t, then use `ido-completing-read' wherever possible.
-Even some places where ido doesn't already enable it."
-  (if (or (not ido-mode)
-          (not ido-everywhere)
-          (boundp 'ido-cur-item)) ; Avoid infinite loop from ido calling completing-read
-      ad-do-it
-    (let ((allcomp (all-completions "" collection predicate)))
-      (if allcomp
-          (setq ad-return-value
-                (ido-completing-read prompt
-                                     allcomp
-                                     nil require-match initial-input hist def))
-        ad-do-it))))
-
-(defadvice find-file-read-args (around disable-ido activate)
-  "Disable ido in find-file"
-  (let ((ido-everywhere nil)) ad-do-it))
+(load "ido-ubiquitous" 'noerror)
 
 (defadvice ido-complete-space (around handle-require-match activate)
   "If require-match is nil, always insert space."
