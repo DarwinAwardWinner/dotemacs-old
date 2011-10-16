23a1a57d960f49308c589ae44652c0f06949bd8a
Ryan C. Thompson <rct@thompsonclan.org>
2011-10-16 04:05:17 -0700

    Remove obsolete hack

--- site-lisp/settings/frame-alist-settings.el
+++ site-lisp/settings/frame-alist-settings.el
@@ -1,4 +1,6 @@
 ;; Add a few things to the default frame alist
-(mapc (apply-partially 'add-to-list 'default-frame-alist)
-      '((font . "Droid Sans Mono-10")
-        (blink-cursor-mode . 1)))
+
+
+;; (mapc (apply-partially 'add-to-list 'default-frame-alist)
+;;       '((font . "Droid Sans Mono-10")
+;;         (blink-cursor-mode . 1)))
