12ff2f1e5057740a726c75a24b1295a9a28a61dd
Ryan C. Thompson <rct@thompsonclan.org>
2011-10-26 00:56:39 -0700

    Overhaul tempbuf-settings.el
    New version is much simpler and should be functionally equivalent, minus a few bugs.

--- site-lisp/settings/tempbuf-settings.el
+++ site-lisp/settings/tempbuf-settings.el
@@ -2,62 +2,37 @@
 (require 'tempbuf)
 (require 'misc-settings)
 
-(defun concat-symbols (&rest symbols)
-  "Like concat, only for symbols instead of strings. Returns a symbol."
-  (intern (apply 'concat (mapcar (lambda (x) (if (symbolp x) (symbol-name x) x)) symbols))))
+(defun mode-symbol (sym)
+  "Append \"-mode\" to SYM unless it already ends in it."
+  (let ((symname (symbol-name sym)))
+    (intern
+     (concat symname
+             (unless (string-ends-with "-mode" symname)
+               "-mode")))))
 
-(defun mode-hook-symbols (modes)
-  "Make an educated guess as to the name of the mode-hooks being
-  referenced in MODES.
+(defun tempbuf-major-mode-hook ()
+  "Turn on `tempbuf-mode' in current buffer if buffer's `major-mode' is in `tempbuf-temporary-major-modes'.
 
-  If MODE ends in `-hook', do nothing; if it ends in `-mode',
-  append `-hook'; else, append `-mode-hook'. Returns a list of symbols.
-  "
-  (mapcar (lambda (mode)
-            (let* ((mode-string (if (stringp mode) mode (symbol-name mode)))
-                   (correct-mode-symbol
-                    (cond ((string-ends-with "-hook" mode-string)
-                           mode)
-                          ((string-ends-with "-mode" mode-string)
-                           (concat-symbols mode "-hook"))
-                          ((concat-symbols mode "-mode-hook")))))
-              correct-mode-symbol))
-          modes))
+Else turn off `tempbuf-mode'."
+  (if (memq major-mode tempbuf-temporary-major-modes)
+      (turn-on-tempbuf-mode)
+    (turn-off-tempbuf-mode)))
 
-(defun add-function-to-multiple-hooks (fun hooks &optional append local)
-  "Take a single function and add it to multiple hooks."
-  (mapc (lambda (hook)
-          (add-hook hook fun append local))
-        hooks))
-
-(defun remove-function-from-multiple-hooks (fun hooks &optional local)
-  "Take a single function and remove it from multiple hooks."
-  (mapc (lambda (hook)
-          (remove-hook hook fun local))
-        hooks))
-
-(defun tempbuf-setup-temporary-major-modes (symbol value)
-  ;; In case some modes were removed from the list, remove the hook.
-  (when (boundp symbol)
-    (let ((oldval (eval symbol)))
-      (remove-function-from-multiple-hooks 'turn-on-tempbuf-mode
-					   (mode-hook-symbols oldval))))
-  ;; Now add the hook to current elements of the list
-  (when value
-    (add-function-to-multiple-hooks 'turn-on-tempbuf-mode
-				    (mode-hook-symbols value)))
-  ;; Tempify already-existing buffers
-  (save-window-excursion
-    (mapc (lambda (buf)
-            (switch-to-buffer buf)
-            (when (memq major-mode value)
-              (turn-on-tempbuf-mode)))
-          (buffer-list)))
-  ;; Lastly, set the value
-  (set-default symbol value))
+(defun tempbuf-setup-temporary-major-modes (symbol newval)
+  (set-default symbol (mapcar 'mode-symbol newval))
+  ;; Set tempbuf-mode correctly in existing buffers.
+  (mapc (lambda (buf)
+          (with-current-buffer buf
+            (tempbuf-major-mode-hook)))
+        (buffer-list)))
 
 (defcustom tempbuf-temporary-major-modes nil
-  "List of major modes that should automatically be marked temporary."
+  "Major modes in which `tempbuf-mode' should be activated.
+
+This will cause buffers of these modes to be automatically killed
+if they are inactive for a short while."
   :group 'tempbuf
   :set 'tempbuf-setup-temporary-major-modes
   :type '(repeat (symbol :tag "Mode")))
+
+(add-hook 'after-change-major-mode-hook 'tempbuf-major-mode-hook)
