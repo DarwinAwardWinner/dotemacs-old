1f959f503cf0f6971f92b990711e41d8d19677b1
Ryan C. Thompson <rct@thompsonclan.org>
2011-11-20 08:29:37 -0800

    Prevent "ffap-guesser" from returning C-style comment openers.
    (Unless a path like "/****" actually exists.)

---
+++ site-lisp/settings/ffap-settings.el
@@ -0,0 +1,12 @@
+(require 'ffap)
+(defvar ffap-c-commment-regexp "^/\\*+"
+  "Matches an opening C-style comment, like \"/***\".")
+
+(defadvice ffap-guesser (after avoid-c-comments activate)
+  "Don't return paths like \"/******\" unless they actually exist."
+  (ignore-errors
+    (when (and ad-return-value
+               (string-match-p ffap-c-commment-regexp
+                               ad-return-value)
+               (not (ffap-file-exists-string ad-return-value)))
+      (setq ad-return-value nil))))
