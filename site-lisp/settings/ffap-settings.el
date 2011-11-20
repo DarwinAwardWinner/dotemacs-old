cafea92da7bd52dbdad926e37b18ab049725a75c
Ryan C. Thompson <rct@thompsonclan.org>
2011-11-20 08:31:47 -0800

    Add comment about rationale.

--- site-lisp/settings/ffap-settings.el
+++ site-lisp/settings/ffap-settings.el
@@ -3,7 +3,10 @@
   "Matches an opening C-style comment, like \"/***\".")
 
 (defadvice ffap-guesser (after avoid-c-comments activate)
-  "Don't return paths like \"/******\" unless they actually exist."
+  "Don't return paths like \"/******\" unless they actually exist.
+
+This fixes the bug where ido would try to suggest a C-style
+comment as a filename."
   (ignore-errors
     (when (and ad-return-value
                (string-match-p ffap-c-commment-regexp
