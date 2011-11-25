3de9c2ac8c2b8e108e3872276f5793deef7cf912
Ryan C. Thompson <rct@thompsonclan.org>
2011-11-25 13:52:03 -0800

    Add "tabify-buffer"

--- site-lisp/settings/untabify-settings.el
+++ site-lisp/settings/untabify-settings.el
@@ -2,3 +2,8 @@
   "Untabify the entire buffer."
   (interactive)
   (untabify (point-min) (point-max)))
+
+(defun tabify-buffer ()
+  "Untabify the entire buffer."
+  (interactive)
+  (tabify (point-min) (point-max)))
