bbc3e28b886d68aced936f0d85d0bc601162b03e
Ryan C. Thompson <rct@thompsonclan.org>
2011-12-17 19:48:51 -0500

    Auto-update IBuffer after killing buffers

--- site-lisp/settings/ibuffer-settings.el
+++ site-lisp/settings/ibuffer-settings.el
@@ -1,6 +1,15 @@
 (require 'ibuffer)
+
+(defvar ibuffer-needs-update nil)
+
 (defadvice kill-buffer (after update-ibuffer activate)
-  (let ((ibuffer-buffer (get-buffer "*Ibuffer*")))
-    (when ibuffer-buffer
-      (with-current-buffer ibuffer-buffer
-        (call-interactively 'ibuffer-update)))))
+  (setq ibuffer-needs-update t)
+  (run-with-idle-timer
+   0.1 nil
+   (lambda ()
+     (when ibuffer-needs-update
+       (let ((ibuffer-buffer (get-buffer "*Ibuffer*")))
+         (when ibuffer-buffer
+           (with-current-buffer ibuffer-buffer
+             (call-interactively 'ibuffer-update))))
+       (setq ibuffer-needs-update nil)))))
