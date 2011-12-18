1ac5a85013c89592185fcee4f756c7135f6b37fe
Ryan C. Thompson <rct@thompsonclan.org>
2011-12-17 19:46:21 -0500

    Prevent mode-compile from overriding "define-obsolete-variable-alias"

--- site-lisp/el-get-init.el
+++ site-lisp/el-get-init.el
@@ -63,7 +63,10 @@
     paredit
     smex
     undo-tree
-    mode-compile
+    (mode-compile
+     :before (lambda ()
+               (when (fboundp 'define-obsolete-variable-alias)
+                 (provide 'obsolete))))
     pymacs
     python-mode
     ipython
