d562257ba90b2f17a195822f3e4252872b889ac7
Ryan C. Thompson <rct@thompsonclan.org>
2012-01-15 12:46:18 -0800

    Remove F11 keybinding for recentf ido
    Ido now already merged recentf entries into the regular buffer list,
    so this is no longer needed.

--- site-lisp/settings/recentf-settings.el
+++ site-lisp/settings/recentf-settings.el
@@ -5,17 +5,3 @@
 
 (global-set-key (kbd "<f12>") 'recentf-open-files)
 
-;; Bind M-F11 to a function that uses ido on the recently opened files
-(defun xsteve-ido-choose-from-recentf ()
-  "Use ido to select a recently opened file from the `recentf-list'"
-  (interactive)
-
-  (let ((home (expand-file-name (getenv "HOME"))))
-    (find-file
-     (ido-completing-read "Recentf open: "
-                          (mapcar (lambda (path)
-                                    (replace-regexp-in-string home "~" path))
-                                  recentf-list)
-                          nil t))))
-
-(global-set-key (kbd "<f11>") 'xsteve-ido-choose-from-recentf)
