f7190fa9683f126b27dac67ac6154b99132137a3
Ryan C. Thompson <rct@thompsonclan.org>
2012-01-15 12:48:16 -0800

    Call recentf-add-file every time a buffer is switched to

--- site-lisp/settings/recentf-settings.el
+++ site-lisp/settings/recentf-settings.el
@@ -5,3 +5,11 @@
 
 (global-set-key (kbd "<f12>") 'recentf-open-files)
 
+;; From http://www.emacswiki.org/emacs/recentf-ext.el:
+;;; [2009/03/01] (@* "`recentf' as most recently USED files")
+(defun recentf-push-buffers-in-frame ()
+  (walk-windows
+   (lambda (win)
+     (let ((bfn (buffer-local-value 'buffer-file-name (window-buffer win))))
+       (and bfn (recentf-add-file bfn))))))
+(add-to-list 'window-configuration-change-hook 'recentf-push-buffers-in-frame)
