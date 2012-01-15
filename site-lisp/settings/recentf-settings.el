b8cef610cd92e3f70d83ab7dbc63ab95a269164c
Ryan C. Thompson <rct@thompsonclan.org>
2012-01-15 12:55:37 -0800

    Fix custom declaration of recentf-max-saved-items

--- site-lisp/settings/recentf-settings.el
+++ site-lisp/settings/recentf-settings.el
@@ -5,6 +5,17 @@
 
 (global-set-key (kbd "<f12>") 'recentf-open-files)
 
+;; Fix custom Declarations
+(eval-after-load 'recentf
+  (progn
+    (defcustom recentf-max-saved-items 20
+      "Maximum number of items of the recent list that will be saved.
+A nil value means to save the whole list.
+See the command `recentf-save-list'."
+      :group 'recentf
+      :type '(choice (const :tag "Save whole list" nil)
+                     (integer :tag "Save this many" 20)))))
+
 ;; From http://www.emacswiki.org/emacs/recentf-ext.el:
 ;;; [2009/03/01] (@* "`recentf' as most recently USED files")
 (defun recentf-push-buffers-in-frame ()
