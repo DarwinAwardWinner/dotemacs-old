cebbdb979bd801c43e8440d61c7b78642b0d34c9
Ryan C. Thompson <rct@thompsonclan.org>
2012-01-16 12:46:23 -0800

    Use ido-yes-or-no

--- site-lisp/settings/quick-yes-settings.el
+++ site-lisp/settings/quick-yes-settings.el
@@ -1,8 +1,4 @@
-(require 'quick-yes)
+;; (require 'quick-yes)
 
-;; Other option: Use Ido
-;; (defadvice yes-or-no-p (around use-ido activate)
-;;   "Use Ido for reading yes or no."
-;;   (let ((full-prompt (concat prompt "(yes or no)")))
-;;     (setq ad-return-value (string= "yes" (ido-completing-read full-prompt '("yes" "no") nil 'require-match)))))
-
+(require 'ido-yes-or-no)
+(ido-yes-or-no-mode 1)
