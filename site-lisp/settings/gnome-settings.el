062634fbc964e358e786ac694698feaf919676c2
Ryan C. Thompson <rct@thompsonclan.org>
2012-01-16 12:47:10 -0800

    Add gnome-settings

---
+++ site-lisp/settings/gnome-settings.el
@@ -0,0 +1,10 @@
+;; http://www.florian-diesch.de/doc/emacs/add-to-gnomes-recently-used-documents/
+(defun fd-add-file-to-recent ()
+  (when buffer-file-name
+    (ignore-errors
+      (start-process "/home/ryan/bin/gnome-addtorecent" nil "addtorecent"
+                     (concat "file://" buffer-file-name)
+                     "text/plain"
+                     "Emacs"
+                     "ec -n %F"))))
+(add-hook 'find-file-hook 'fd-add-file-to-recent)
