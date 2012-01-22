2bd5564880347ddfce474b1131669d132679da94
Ryan C. Thompson <rct@thompsonclan.org>
2012-01-22 15:35:01 -0800

    Make git-commit-mode work with git rebase/emacsclient

---
+++ site-lisp/settings/git-commit-settings.el
@@ -0,0 +1,8 @@
+(defadvice git-commit--save-and-exit (around handle-server-mode activate)
+  (if server-buffer-clients
+      ;; Server-mode
+      (progn
+        (save-buffer)
+        (server-edit))
+    ;; Non-server-mode
+    ad-do-it))
