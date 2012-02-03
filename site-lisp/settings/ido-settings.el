f99b6ecfe711dbee6f0ef9c40c47e63443037523
Ryan C. Thompson <rct@thompsonclan.org>
2012-02-03 12:17:40 -0800

    Force ido-yes-or-no mode on since preference doesn't seem to stick

--- site-lisp/settings/ido-settings.el
+++ site-lisp/settings/ido-settings.el
@@ -1,6 +1,7 @@
 (require 'ido)
 (load "ido-other-window" 'noerror)
-(load "ido-yes-or-no" 'noerror)
+(when (load "ido-yes-or-no" 'noerror)
+  (ido-yes-or-no-mode 1))
 
 (defadvice ido-complete-space (around handle-require-match activate)
   "If require-match is nil, always insert space."
