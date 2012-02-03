3d901319773873dcac5caa7e4a01d8337d6bb2ef
Ryan C. Thompson <rct@thompsonclan.org>
2012-02-03 12:17:56 -0800

    Require doc-mode for asciidoc

--- site-lisp/settings/asciidoc-settings.el
+++ site-lisp/settings/asciidoc-settings.el
@@ -3,3 +3,4 @@
      (add-to-list 'auto-mode-alist '("\\.[Aa][Dd][Oo][Cc]$" . doc-mode))
      (add-to-list 'auto-mode-alist '("\\.[Aa][Ss][Cc][Ii][Ii][Dd][Oo][Cc]$" . doc-mode))))
 (require 'asciidoc nil 'noerror)
+(require 'doc-mode)
