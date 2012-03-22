ab1c871a47ac14ef877a08e2089644f93e1c7706
Ryan C. Thompson <rct@thompsonclan.org>
2012-03-22 16:58:42 -0700

    Eval machine-specific-settings after init

--- site-lisp/settings/machine-specific-settings.el
+++ site-lisp/settings/machine-specific-settings.el
@@ -1,16 +1,17 @@
 ;; Settings for computers in the Salomon lab
 
-(case (intern system-name)
+(eval-after-load "init"
+  (case (intern system-name)
 
-  ;; My laptop
-  (aeolus nil)
+    ;; My laptop
+    (aeolus nil)
 
-  ;; Salomon workstation
-  (ubuntu
-   (set-face-attribute 'default nil :height 105)
-   (setq magit-repo-dirs
-         (cons "~/Projects"
-               (remove-if (apply-partially 'string-match-p
-                                           "~/Projects/")
-                          magit-repo-dirs))
-         clean-buffer-list-delay-general 7)))
+    ;; Salomon workstation
+    (ubuntu
+     (set-face-attribute 'default nil :height 105)
+     (setq magit-repo-dirs
+           (cons "~/Projects"
+                 (remove-if (apply-partially 'string-match-p
+                                             "~/Projects/")
+                            magit-repo-dirs))
+           clean-buffer-list-delay-general 7))))
