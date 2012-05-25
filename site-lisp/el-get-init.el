a214f2c98e34e2bc20045cd6fcb30fe44e17a480
Ryan C. Thompson <rct@thompsonclan.org>
2012-05-25 13:01:01 -0700

    Avoid cvs for wanderlust, emacs-goodies, etc.
    Instead define whole new recipes that download tarballs or use some
    other method.
    
    Because cvs is awful and never works ever.

--- site-lisp/el-get-init.el
+++ site-lisp/el-get-init.el
@@ -25,7 +25,6 @@
   ;; Packages to install from existing recipes
   (setq el-get-sources-from-recipes
         '(anything
-          apel
           auto-complete
           auto-install
           autopair
@@ -65,7 +64,6 @@
           remember
           revive-plus
           js2-mode
-          wanderlust
           ))
 
   ;; Packages to install via elpa
@@ -143,6 +141,191 @@
                       :type http
                       :url ,url))
             el-get-sources-from-http)
+
+    ;; Can't use the builtin recipes because cvs is awful
+    '((:name apel
+            :website "http://www.kanji.zinbun.kyoto-u.ac.jp/~tomo/elisp/APEL/"
+            :description "APEL (A Portable Emacs Library) is a library to support to write portable Emacs Lisp programs."
+            :type http-tar
+            :module "apel"
+	    :url "http://archive.ubuntu.com/ubuntu/pool/universe/a/apel/apel_10.8.orig.tar.gz"
+            :options ("xzf")
+            :build
+            (mapcar
+             (lambda (target)
+               (list el-get-emacs
+                     (split-string "-batch -q -no-site-file -l APEL-MK -f")
+                     target
+                     "prefix" "site-lisp" "site-lisp"))
+             '("compile-apel" "install-apel"))
+            :load-path ("site-lisp/apel" "site-lisp/emu"))
+      (:name flim
+             :description "A library to provide basic features about message representation or encoding"
+             :depends apel
+             :type http-tar
+             :url "http://archive.ubuntu.com/ubuntu/pool/universe/f/flim/flim_1.14.9+0.20110516.orig.tar.gz"
+             :options ("xzf")
+             ;; :type cvs
+             ;; :module "flim"
+             ;; :url ":pserver:anonymous@cvs.m17n.org:/cvs/root"
+             :build
+             (mapcar
+              (lambda (target)
+                (list el-get-emacs
+                      (mapcar (lambda (pkg)
+                                (mapcar (lambda (d) `("-L" ,d)) (el-get-load-path pkg)))
+                              '("apel"))
+                      (split-string "-batch -q -no-site-file -l FLIM-MK -f")
+                      target
+                      "prefix" "site-lisp" "site-lisp"))
+              '("compile-flim" "install-flim"))
+             :load-path ("site-lisp/flim"))
+      (:name semi
+             :description "SEMI is a library to provide MIME feature for GNU Emacs."
+             :depends flim
+             :type http-tar
+             :url "http://archive.ubuntu.com/ubuntu/pool/universe/s/semi/semi_1.14.6+0.20101114.orig.tar.gz"
+             :options ("xzf")
+             ;; :type cvs
+             ;; :module "semi"
+             ;; :url ":pserver:anonymous@cvs.m17n.org:/cvs/root"
+             :build
+             (mapcar
+              (lambda (target)
+                (list el-get-emacs
+                      (mapcar (lambda (pkg)
+                                (mapcar (lambda (d) `("-L" ,d)) (el-get-load-path pkg)))
+                              '("apel" "flim"))
+                      (split-string "-batch -q -no-site-file -l SEMI-MK -f")
+                      target
+                      "prefix" "site-lisp" "site-lisp"))
+              '("compile-semi" "install-semi"))
+             :load-path ("site-lisp/semi/"))
+      (:name wanderlust
+       :description "Wanderlust bootstrap."
+       :depends semi
+       :type http-tar
+       ;; :url "file:///home/ryan/temp/wl_2.14.0.orig.tar.gz"
+       :url "http://archive.ubuntu.com/ubuntu/pool/universe/w/wl/wl_2.14.0.orig.tar.gz"
+       :options ("xzf")
+       ;; :type cvs
+       ;; :module "wanderlust"
+       ;; :url ":pserver:anonymous@cvs.m17n.org:/cvs/root"
+       :build
+       (mapcar
+        (lambda (target-and-dirs)
+          (list el-get-emacs
+                (mapcar (lambda (pkg)
+                          (mapcar (lambda (d) `("-L" ,d)) (el-get-load-path pkg)))
+                        (append
+                         '("apel" "flim" "semi")
+                         (when (el-get-package-exists-p "bbdb") (list "bbdb"))))
+		;; Force the coding system to utf-8 so that Emacs
+		;; doesn't try to prompt for it so it can run
+		;; noninteractively.
+		"--eval" (el-get-print-to-string
+			  '(defun select-safe-coding-system (&rest ignored) (quote utf-8)))
+                "--eval" (el-get-print-to-string
+                          '(progn (setq wl-install-utils t)
+                                  (setq wl-info-lang "en")
+                                  (setq wl-news-lang "en")))
+
+                (split-string "-batch -q -no-site-file -l WL-MK -f")
+                target-and-dirs))
+        '(("compile-wl-package"  "site-lisp" "icons")
+          ("install-wl-package" "site-lisp" "icons")
+	  ("wl-texinfo-format" "doc")))
+       :info "doc/wl.info"
+       :load-path ("site-lisp/wl" "utils"))
+      (:name emacs-goodies-el
+             :website "http://packages.debian.org/sid/emacs-goodies-el"
+             :description "Miscellaneous add-ons for Emacs"
+             :type http-tar
+             :url "http://alioth.debian.org/snapshots.php?group_id=30060"
+             :options ("xzf")
+             :build
+             (let ((makerfiles
+                    (split-string (shell-command-to-string "find . -name '*.make'"))))
+               (message "MAKERFILES: %S" makerfiles)
+               (mapcar
+                (lambda (makerfile)
+                  (let ((maker-dir (file-name-directory makerfile))
+                        (maker-command
+                         (replace-regexp-in-string
+                          "\n" ""
+                          (replace-regexp-in-string
+                           "^emacs" el-get-emacs
+                           (with-temp-buffer
+                             (insert-file-contents makerfile)
+                             (buffer-string))))))
+                    (format "cd %s && %s" maker-dir maker-command)))
+                makerfiles))
+             :load-path ("emacs-goodies-el/elisp/debian-el"
+                         "emacs-goodies-el/elisp/devscripts-el"
+                         "emacs-goodies-el/elisp/dpkg-dev-el"
+                         "emacs-goodies-el/elisp/emacs-goodies-el"
+                         ;; "emacs-goodies-el/elisp/gnus-bonus-el"
+                         "emacs-goodies-el/elisp/vm-bonus-el")
+             ;; The :features and :post-init are taken from the
+             ;; debain/*.emacsen-startup files in the source package.
+             ;; Some of these files just require specific features,
+             ;; and those are added to :features. Others have some
+             ;; significant init code, and this has been manually
+             ;; copied into :post-init. The emacsen-startup files
+             ;; cannot be used directly because they have hardcoded
+             ;; paths to where dpkg would install the packages.
+             :features (emacs-goodies-el
+                        debian-el
+                        dpkg-dev-el)
+             :post-init
+             (progn
+                          ;; autoloads for devscripts.el
+                          (autoload 'debuild "devscripts" "Run debuild in the current directory." t)
+                          (autoload 'debc "devscripts" "Run debc in the current directory." t)
+                          (autoload 'debi "devscripts" "Run debi in the current directory." t)
+                          (autoload 'debit "devscripts" "Run debit in the current directory." t)
+                          (autoload 'debdiff "devscripts" "Compare contents of CHANGES-FILE-1 and CHANGES-FILE-2." t)
+                          (autoload 'debdiff-current "devscripts"   "Compare the contents of .changes file of current version with previous version;
+requires access to debian/changelog, and being in debian/ dir." t)
+                          (autoload 'debclean "devscripts" "Run debclean in the current directory." t)
+                          (autoload 'pdebuild "pbuilder-mode" "Run pdebuild in the current directory." t)
+                          (autoload 'pdebuild-user-mode-linux "pbuilder-mode" "Run pdebuild-user-mode-linux in the current directory." t)
+                          (autoload 'pbuilder-log-view-elserv "pbuilder-log-view-mode" "Run a elserv session with log view.
+
+Running this requires elserv.  Use elserv, and do `elserv-start' before invoking this command." t)
+                          (autoload 'debuild-pbuilder "pbuilder-mode" "Run debuild-pbuilder in the current directory." t)
+                          (autoload 'pbuilder-build "pbuilder-mode" "Run pbuilder-build for the given filename." t)
+                          (autoload 'pbuilder-user-mode-linux-build "pbuilder-mode" "Run pbuilder-user-mode-linux for the given filename." t)
+
+                          ;; from "debian/vm-bonus-el.emacsen-startup"
+                          (defgroup vm-bonus-el nil
+                            "Customize vm-bonus-el Debian packages."
+                            :group 'vm)
+
+                          ;; vm-bogofilter.el
+                          (defgroup vm-bogofilter nil
+                            "VM Spam Filter Options"
+                            :group 'vm
+                            :group 'vm-bonus-el
+                            :load 'vm-bogofilter)
+
+                          (autoload 'vm-bogofilter-setup "vm-bogofilter"
+                            "Initialize vm-bogofilter."
+                            t)
+
+                          (defcustom vm-bogofilter-setup nil
+                            "Whether to initialize vm-bogofilter on startup.
+vm-bogofilter interfaces VM with the bogofilter spam filter."
+                            :type 'boolean
+                            :set (lambda (symbol value)
+                                   (set-default symbol value)
+                                   (when value
+                                     (vm-bogofilter-setup)))
+                            :load 'vm-bogofilter
+                            :group 'vm
+                            :group 'vm-bogofilter
+                            :group 'vm-bonus-el))))
+
 
     ;; For testing purposes
     '((:name fake-test-pkg
