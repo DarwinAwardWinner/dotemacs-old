db7671a932278c5c5529d0b41c68f90eb96afba4
Ryan C. Thompson <rct@thompsonclan.org>
2012-05-25 13:33:33 -0700

    Remove unneeded section from wl-settings

--- site-lisp/settings/wl-settings.el
+++ site-lisp/settings/wl-settings.el
@@ -37,12 +37,3 @@
 
 ;; Use wl for M-x compose-mail
 (autoload 'wl-user-agent-compose "wl-draft" nil t)
-(if (boundp 'mail-user-agent)
-    (setq mail-user-agent 'wl-user-agent))
-(if (fboundp 'define-mail-user-agent)
-    (define-mail-user-agent
-      'wl-user-agent
-      'wl-user-agent-compose
-      'wl-draft-send
-      'wl-draft-kill
-      'mail-send-hook))
