4dd8f17dc843b74d8f46b6cd8a2f24f5040f8008
Ryan C. Thompson <rct@thompsonclan.org>
2012-05-25 13:35:01 -0700

    Fix inappropriate evaluation in saveplace-settings
    Forgot to quote something

--- site-lisp/settings/saveplace-settings.el
+++ site-lisp/settings/saveplace-settings.el
@@ -1,11 +1,10 @@
-;;(setq-default save-place t)                   ;; activate it for all buffers
-;;(require 'saveplace)                          ;; get the package
+(require 'saveplace)
 
 ;; Fix a problem with saveplace.el putting you back in a folded position:
 (eval-after-load "org"
-  (add-hook 'org-mode-hook
-            (lambda ()
-              (when (outline-invisible-p)
-                (save-excursion
-                  (outline-previous-visible-heading 1)
-                  (org-show-subtree))))))
+  '(add-hook 'org-mode-hook
+             (lambda ()
+               (when (outline-invisible-p)
+                 (save-excursion
+                   (outline-previous-visible-heading 1)
+                   (org-show-subtree))))))
