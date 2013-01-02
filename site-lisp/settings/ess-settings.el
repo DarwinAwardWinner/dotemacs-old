c1208629d50602eaf6825b7005cb3717f559b55b
Ryan C. Thompson <rct@thompsonclan.org>
2013-01-02 16:07:26 -0500

    Set my own ESS indentation style

--- site-lisp/settings/ess-settings.el
+++ site-lisp/settings/ess-settings.el
@@ -6,8 +6,19 @@
   '(add-hook 'ess-help-mode-hook
              'turn-on-tempbuf-mode))
 
-;; Filladapt and ess break each other.
+;; Filladapt and ess seem to break each other.
 (eval-after-load "ess"
   '(load "fill"))
 (eval-after-load "filladapt"
   '(load "fill"))
+
+;; Set my own ESS indentation style
+(eval-after-load "ess"
+  (progn
+    (setq ess-default-style 'OWN)
+    (setq ess-own-style-list
+          (cons (cons 'ess-arg-function-offset-new-line '(4))
+                (remove-if
+                 (lambda (x)
+                   (eq (car x) 'ess-arg-function-offset-new-line))
+                 (cdr (assoc 'C++ ess-style-alist)))))))
